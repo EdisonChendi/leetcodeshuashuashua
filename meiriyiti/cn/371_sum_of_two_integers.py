4ac1f7c6ee2503484ab7bd54f2bf86de8c4df8fd
chendi <jokerchendi@gmail.com>
2021-09-28 01:10:38 +0800

    [meiriyiti][bit manipulation]371_sum_of_two_integers

---
+++ meiriyiti/cn/371_sum_of_two_integers.py
@@ -0,0 +1,22 @@
+import unittest
+from typing import List
+from pprint import pprint
+
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        pass
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_case_1(self):
+        sol = Solution()
+        pass
+
+    # def test_edge_case_1(self):
+    #     sol = Solution()
+
+
+if __name__ == "__main__":
+    unittest.main()
