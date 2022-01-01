4547fe0b8156f5061aed6823d7304ad107112e68
chendi <jokerchendi@gmail.com>
2022-01-01 12:05:59 +0800

    [meiriyiti]472_concatednated_words.py
    1.补
    2.hard
    3.trie变形

--- meiriyiti/cn/472_concatednated_words.py
+++ meiriyiti/cn/472_concatednated_words.py
@@ -1,11 +1,97 @@
 import unittest
 from typing import List
 from pprint import pprint
+import collections
+
+
+class TrieNode:
+
+    def __init__(self) -> None:
+        self.end_of_word = False
+        self.children = collections.defaultdict(TrieNode)
+
+
+class Trie:
+
+    def __init__(self) -> None:
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for ch in word:
+            node = node.children[ch]
+        node.end_of_word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return node.end_of_word
+
+    def dfs(self, word: str, idx: int) -> bool:
+        if idx == len(word):
+            return True
+
+        node = self.root
+        for i in range(idx, len(word)):
+            ch = word[i]
+            if ch not in node.children:
+                return False
+
+            node = node.children[ch]
+            if node.end_of_word and self.dfs(word, i+1):
+                return True
+
+        return False
 
 
 class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
-        pass
+        res = []
+
+        words.sort(key=len)
+        trie = Trie()
+
+        for w in words:
+            if not w:
+                continue
+            if trie.dfs(w, 0):
+                res.append(w)
+            else:
+                trie.insert(w)
+
+        return res
+
+
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        res = []
+
+        words.sort(key=len)
+        trie = Trie()
+
+        for w in words:
+            if not w:
+                continue
+            if trie.dfs(w, 0):
+                res.append(w)
+            else:
+                trie.insert(w)
+
+        return res
+
+
+class TestTrieSolution(unittest.TestCase):
+
+    def test_case_1(self):
+        trie = Trie()
+        trie.insert("apple")
+        self.assertTrue(trie.search("apple"))
+        self.assertFalse(trie.search("app"))
+        trie.insert("app")
+        self.assertTrue(trie.search("app"))
 
 
 class TestSolution(unittest.TestCase):
