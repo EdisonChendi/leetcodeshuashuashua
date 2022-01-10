e412452b153f698c88bd86c01b80a6290b1724b2
chendi <jokerchendi@gmail.com>
2022-01-10 23:39:29 +0800

    [meiriyiti]306_additive_number
    1.get all combinations of 1st number and 2nd number, and check whether
      the reset is valid

---
+++ meiriyiti/cn/306_additive_number.py
@@ -0,0 +1,91 @@
+import unittest
+from typing import List
+from pprint import pprint
+
+
+class Solution:
+    def isAdditiveNumber(self, num: str) -> bool:
+
+        def add(s1: str, s2: str) -> str:
+            l1 = len(s1)
+            l2 = len(s2)
+            i = max(l1, l2)
+            s1 = '0'*(i-l1) + s1
+            s2 = '0'*(i-l2) + s2
+            carry = 0
+            res = []
+            for i in reversed(range(i)):
+                carry, d = divmod(carry + int(s1[i]) + int(s2[i]), 10)
+                res.append(str(d))
+            if carry:
+                res.append(str(carry))
+            return ''.join(reversed(res))
+
+        def backtrack(p1, p2, num):
+            if not num:
+                return True
+
+            i = max(len(p1), len(p2))
+            if len(num) < i:
+                return False
+
+            s = add(p1, p2)
+            ni = num[:i]
+            if len(ni) > 1 and ni[0] == '0':
+                return False
+            if len(s) == len(ni):
+                if ni == s:
+                    return backtrack(p2, ni, num[i:])
+            elif len(num) > i:
+                ni = num[:i+1]
+                return ni[0] != '0' and ni == s and backtrack(p2, ni, num[i+1:])
+            return False
+
+        N = len(num)
+        if N <= 2:
+            return False
+        for i in range(1, N//2+1):
+            p1 = num[:i]
+            if len(p1) > 1 and p1[0] == '0':
+                break
+            for j in range(i, min(i+N//2+1, N-i)):
+                p2 = num[i:j+1]
+                if len(p2) > 1 and p2[0] == '0':
+                    break
+                if backtrack(p1, p2, num[j+1:]):
+                    return True
+        return False
+
+
+class TestSolution(unittest.TestCase):
+
+    # def test_case_1(self):
+    #     sol = Solution()
+    #     num = "112358"
+    #     expected = True
+    #     self.assertEqual(sol.isAdditiveNumber(num), expected)
+
+    def test_case_2(self):
+        sol = Solution()
+        num = "199100199"
+        expected = True
+        self.assertEqual(sol.isAdditiveNumber(num), expected)
+
+    # def test_case_3(self):
+    #     sol = Solution()
+    #     num = "113"
+    #     expected = False
+    #     self.assertEqual(sol.isAdditiveNumber(num), expected)
+
+    # def test_case_4(self):
+    #     sol = Solution()
+    #     num = "198019823962"
+    #     expected = True
+    #     self.assertEqual(sol.isAdditiveNumber(num), expected)
+
+    # def test_edge_case_1(self):
+    #     sol = Solution()
+
+
+if __name__ == "__main__":
+    unittest.main()
