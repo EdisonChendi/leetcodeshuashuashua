0dbd93c117647f76d12d1d8b05ef1b30cdc507d2
chendi <jokerchendi@gmail.com>
2022-01-23 18:29:15 +0800

    [topics][dp][hard]132_palindrome_partitioning_ii
    1.run dp twice
    2.about dp transition function:
        first, try simple one(usually one-dimension)
        if not solvable, try increase dimension

--- topics/dynamic_programming/leetbook_cn/132_palindrome_partitioning_ii.py
+++ topics/dynamic_programming/leetbook_cn/132_palindrome_partitioning_ii.py
@@ -3,34 +3,52 @@ from typing import List
 from pprint import pprint
 
 
-class Solution:
+class Solution1:
     def minCut(self, s: str) -> int:
         # dp[i][j] = min(dp[i][k]+dp[k+1][j]) for k in range(i,j)
         cache = {}
 
-        def palindrome(i,j):
-            if (i,j) in cache:
-                return cache[(i,j)]
+        def palindrome(i, j):
+            if (i, j) in cache:
+                return cache[(i, j)]
             if i >= j:
                 return True
             if s[i] == s[j]:
-                res = palindrome(i+1,j-1)
+                res = palindrome(i+1, j-1)
             else:
                 res = False
-            cache[(i,j)] = res
+            cache[(i, j)] = res
             return res
 
-
         L = len(s)
-        dp = [0,]*L
+        dp = [0, ]*L
 
         for j in range(L):
-            if palindrome(0,j):
+            if palindrome(0, j):
                 dp[j] = 0
             else:
-                dp[j] = min(1+dp[i-1] for i in range(1,j+1) if palindrome(i,j))
-            
-        
+                dp[j] = min(1+dp[i-1]
+                            for i in range(1, j+1) if palindrome(i, j))
+
+        return dp[-1]
+
+
+class Solution:
+    def minCut(self, s: str) -> int:
+        L = len(s)
+        palindrome = [[True]*L for _ in range(L)]
+        for i in reversed(range(L)):
+            for j in range(i+1, L):
+                palindrome[i][j] = s[j] == s[i] and \
+                    (j == i+1 or palindrome[i+1][j-1])
+
+        dp = list(range(L))
+        for i in range(1, L):
+            if palindrome[0][i]:
+                dp[i] = 0
+            else:
+                dp[i] = min(1+dp[j-1]
+                            for j in range(1, i+1) if palindrome[j][i])
         return dp[-1]
 
 
