eb8dc0321e0a9f0627bd8b23f44d68aa9c0f35e6
chendi <jokerchendi@gmail.com>
2022-04-16 21:11:37 +0800

    [meiriyiti][us]479_largest_palindrome_product
    1.read carefully
    2.brain stuck
    3.do it again

---
+++ meiriyiti/us/669_trim_a_binary_search_tree.py
@@ -0,0 +1,45 @@
+import unittest
+from typing import List
+from pprint import pprint
+
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+
+        def trim(node):
+            if not node:
+                return
+
+            if node.val < low:
+                return trim(node.right)
+            elif node.val > high:
+                return trim(node.left)
+            else:
+                node.left = trim(node.left)
+                node.right = trim(node.right)
+                return node
+
+        return trim(root)
+
+
+class TestSolution(unittest.TestCase):
+
+    def test_case_1(self):
+        sol = Solution()
+        pass
+
+    # def test_edge_case_1(self):
+    #     sol = Solution()
+
+
+if __name__ == "__main__":
+    unittest.main()
